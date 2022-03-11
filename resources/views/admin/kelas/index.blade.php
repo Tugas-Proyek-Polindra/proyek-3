0d3ffc4747e40dab12f0549bb3c029059b659838
kamaludinnoviyanto <landofnoangels@gmail.com>
2022-03-11 11:40:43 +0700

    nambah view kelas

---
+++ resources/views/admin/kelas/index.blade.php
@@ -0,0 +1,19 @@
+@extends('adminlte::page')
+
+@section('title', 'Kelas')
+
+@section('content_header')
+    <h1>Kelas</h1>
+@stop
+
+@section('content')
+    <p>Haloo</p>
+@stop
+
+@section('css')
+    <link rel="stylesheet" href="/css/admin_custom.css">
+@stop
+
+@section('js')
+    <script> console.log('Hi!'); </script>
+@stop

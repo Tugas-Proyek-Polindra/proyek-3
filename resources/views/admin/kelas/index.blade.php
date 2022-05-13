bdec7c989b80cafe2d7d04379f5aa87a87821966
khuzaemah <khuza.emah24@gmail.com>
2022-05-13 11:29:46 +0700

    revisi fitur kelas

--- resources/views/admin/kelas/index.blade.php
+++ resources/views/admin/kelas/index.blade.php
@@ -1,64 +1,64 @@
 @extends('adminlte::page')
 @section('title', 'Kelas')
 @section('content_header')
-    <h1 class="m-0 text-dark">Tabel @yield('title')</h1>
+<div class="row mb-2">
+    <div class="col-sm-6">
+        <h1>Tabel @yield('title')</h1>
+    </div>
+    <div class="col-sm-6">
+        <ol class="breadcrumb float-sm-right">
+        <li class="breadcrumb-item"><a href="/">Home</a></li>
+        <li class="breadcrumb-item active"> @yield('title')</li>
+        </ol>
+    </div>
+    </div>
 @stop
 @section('content')
     <div class="row">
         <div class="col-12">
+
+            @if (session('pesan'))
+            <div class="alert alert-success alert-dismissible">
+                <button type="button" class="close" data-dismis="alert" aria-hidden="true">&times;</button>
+                <h4><i class="icon fa fa-check"></i>Success:</h4>
+                {{session('pesan')}}
+            </div>     
+            @endif
+
             <div class="card">
                 <div class="card-body">
                     <a href="/kelas/create" class="btn btn-primary mb-2"><i class="fa fa-fw fa-plus-square"></i>Tambah Data</a>
                     <table class="table table-hover table-bordered table-stripped" id="example2">
                         <thead>
                         <tr>
-                          <th>Kelas</th>
-                          <th>Wali Kelas</th>
-                          <th>Tahun Ajaran</th>
-                          <th>Aksi</th>
+                        <th>Kelas</th>
+                        <th>Wali Kelas</th>
+                        <th>Tahun Ajaran</th>
+                        <th>Aksi</th>
                         </tr>
                         </thead>
                         <tbody>
-                          @foreach ($classes as $kelas)
-                          <tr>
-                            <td>{{$kelas->kelas}}</td>
-                            <td>{{$kelas->wali_kelas}}</td>
-                            <td>{{$kelas->tahun_ajaran}}</td>
-                            <td >                                        
-                              <form method="post" action="/kelas/{{$kelas->id}}" class="form-inline">
-                                  <a href="/kelas/{{$kelas->id}}" class="btn btn-sm btn-success" ><i class="fas fa-eye"></i></a>
-                                  <a href="/kelas/{{$kelas->id}}/edit" class="btn btn-sm btn-warning" ><i class="fas fa-edit"></i></a>
-                                  @csrf
-                                  @method('delete')
-                                  <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete "><i class="fas fa-trash"></i></button>    
-                              </form>           
-                                <div class="modal modal-danger fade" id="delete ">
-                                  <div class="modal-dialog modal-sm">
-                                  <div class="modal-content">
-                                    <div class="modal-header">
-                                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                          <span aria-hidden="true">&times;</span></button>
-                                      <h4 class="modal-title"></h4>
-                                      </div>
-                                      <div class="modal-body">
-                                      <p>Apakah Anda Yakin Ingin Menghapus Data Ini....???</p>
-                                      </div>
-                                      <div class="modal-footer">
-                                      <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">No</button>
-                                      <button type="button" class="btn btn-outline pull-right">Yes</button>
-                                    </div>
-                                  </div>
-                                  </div>
-                              </div>
+                        @foreach ($classes as $kelas)
+                        <tr>
+                            <td width="10%">{{$kelas->kelas}}</td>
+                            <td width="25%">{{$kelas->wali_kelas}}</td>
+                            <td width="15%">{{$kelas->tahun_ajaran}}</td>
+                            <td width="5%">
+                                <div class="btn btn-group">
+                                <a href="/kelas/{{$kelas->id}}" class="btn btn-sm btn-success" ><i class="fas fa-eye"></i></a>
+                                <a href="/kelas/{{$kelas->id}}/edit" class="btn btn-sm btn-warning" ><i class="fas fa-edit"></i></a>
+                                <a href="{{route('kelas.destroy', $kelas)}}" onclick="notificationBeforeDelete(event, this)" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></a>
+                                </div>
                             </td>
-                          </tr>
-                          @endforeach
+                        </tr>
+                        @endforeach
                         </tbody>
                     </table>
                 </div>
             </div>
         </div>
     </div>
+
 @stop
 @push('js')
     <form action="" id="delete-form" method="post">

2b3ad1b6fca016a55be41260dfdcec56658e8598
kurniaastrinf <kurniaastrin@gmail.com>
2022-06-03 19:42:37 +0700

    biodataguru

--- resources/views/admin/guru/index.blade.php
+++ resources/views/admin/guru/index.blade.php
@@ -1,5 +1,4 @@
 @extends('adminlte::page')
-
 @section('title', 'Biodata Guru')
 @section('content_header')
 <div class="row mb-2">
@@ -31,21 +30,22 @@
                     <a href="/guru/create" class="btn btn-primary mb-2"><i class="fa fa-fw fa-plus-square"></i>Tambah Data</a>
                     <table class="table table-hover table-bordered table-stripped" id="example2">
                         <thead>
-                        <tr>
-                            <th>No</th>
-                            <th>Nama Guru</th>
-                            <th>NIK</th>
-                            <th>Tempat Lahir</th>
-                            <th>Tgl Lahir</th>
-                            <th>Jenis Kelamin</th>
-                            <th>No.HP</th>
-                            {{-- <th>Nama Ibu</th>
-                            <th>Status Pegawai</th>
-                            <th>Pendidikan Terakhir</th> --}}
-                        </tr>
+                            <tr>
+                                <th>No</th>
+                                <th>Nama Guru</th>
+                                <th>NIK</th>
+                                <th>Tempat Lahir</th>
+                                <th>Tgl Lahir</th>
+                                <th>Jenis Kelamin</th>
+                                <th>No.HP</th>
+                                {{-- <th>Nama Ibu</th>
+                                <th>Status Pegawai</th>
+                                <th>Pendidikan Terakhir</th> --}}
+                                <th>Aksi</th>
+                            </tr>
                         </thead>
                         <tbody>
-                        @foreach ($guru->skip(1) as $guru)
+                            @foreach ($guru->skip(1) as $guru)
                         <tr>
                             <td> {{$loop->iteration}} </td>
                             <td>{{$guru->name}}</td>
@@ -57,45 +57,23 @@
                             {{-- <td>{{$guru->nama_ibu}}</td>
                             <td>{{$guru->status_pegawai}}</td>
                             <td>{{$guru->pendidikan_terakhir}}</td> --}}
-                            <td widht= "5%">
-                        <form method="post" action="/guru/{{$guru->id}}" class="form-inline">
-                            <a href="/guru/{{$guru->id}}" class="btn btn-sm btn-success" ><i class="fas fa-eye"></i></a>
-                            <a href="/guru/{{$guru->id}}/edit" class="btn btn-sm btn-warning" ><i class="fas fa-edit"></i></a>
-                            @csrf
-                            @method('delete')
-                            <button class="btn btn-sm btn-danger" data-toggle="modal" data-target="#delete "><i class="fas fa-trash"></i></button>
-                        </form>
-                        <div class="modal modal-danger fade" id="delete ">
-                            <div class="modal-dialog modal-sm">
-                            <div class="modal-content">
-                                <div class="modal-header">
-                                    <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                        <span aria-hidden="true">&times;</span></button>
-                                    <h4 class="modal-title"></h4>
-                                    </div>
-                                    <div class="modal-body">
-                                    <p>Apakah Anda Yakin Ingin Menghapus Data Ini....???</p>
-                                    </div>
-                                    <div class="modal-footer">
-                                    <button type="button" class="btn btn-outline pull-left" data-dismiss="modal">No</button>
-                                    <button type="button" class="btn btn-outline pull-right">Yes</button>
+                            <td width="5%">
+                                <div class="btn btn-group">
+                                    <a href="/guru/{{$guru->id}}" class="btn btn-sm btn-success" ><i class="fas fa-eye"></i></a>
+                                    <a href="/guru/{{$guru->id}}/edit" class="btn btn-sm btn-warning" ><i class="fas fa-edit"></i></a>
+                                <a href="{{route('guru.destroy', $guru)}}" onclick="notificationBeforeDelete(event, this)" class="btn btn-sm btn-danger"><i class="fas fa-trash"></i></a>
                                 </div>
-                            </div>
-                            </div>
-                        </div>
-                    </td>
-                </tr>
-                    @endforeach
-                </tbody>
-            </table>
+                            </td>
+                        </tr>
+                        @endforeach
+                        </tbody>
+                    </table>
+                </div>
             </div>
-        </div>
         </div>
     </div>
 
 @stop
-
-
 @push('js')
     <form action="" id="delete-form" method="post">
         @method('delete')

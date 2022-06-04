a2b6ecea1dd9aa2106c893b27946a09988b86586
kurniaastrinf <kurniaastrin@gmail.com>
2022-06-04 20:49:29 +0700

    isi tabel

--- database/seeders/NilaiPelajaranSeeder.php
+++ database/seeders/NilaiPelajaranSeeder.php
@@ -25,6 +25,106 @@ class NilaiPelajaranSeeder extends Seeder
             'b_arab' => '90',
             'praktikum' => '89',
         ]);
+        NilaiPelajaran::create([
+            'santri_id' => 2,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '80',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 3,
+            'al_quran' => '80',
+            'al_hadist' => '98',
+            'aqidah' => '84',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 4,
+            'al_quran' => '80',
+            'al_hadist' => '99',
+            'aqidah' => '84',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 5,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '84',
+            'akhlaq' => '80',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 6,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '80',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 7,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '80',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 8,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '80',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 9,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '80',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+        NilaiPelajaran::create([
+            'santri_id' => 10,
+            'al_quran' => '80',
+            'al_hadist' => '90',
+            'aqidah' => '80',
+            'akhlaq' => '87',
+            'fiqih' => '83',
+            'tarikh' => '93',
+            'b_arab' => '90',
+            'praktikum' => '89',
+        ]);
+
 
     }
 }
